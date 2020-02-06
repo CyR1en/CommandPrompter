0ccbbd04657707162cecb25f24a49b565dc25418
CyR1en <cyrien3519@gmail.com>
2020-02-05 21:49:37 -0700

    null safety

--- src/main/java/com/cyr1en/cp/command/CommandManager.java
+++ src/main/java/com/cyr1en/cp/command/CommandManager.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019 Ethan Bacurio
+ * Copyright (c) 2020 Ethan Bacurio
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -54,7 +54,7 @@ public class CommandManager implements CommandExecutor {
 
   public void registerTabCompleter(CommandTabCompleter commandTabCompleter) {
     PluginCommand command = Bukkit.getPluginCommand("commandprompter");
-    command.setTabCompleter(commandTabCompleter);
+    Objects.requireNonNull(command).setTabCompleter(commandTabCompleter);
   }
 
   @Override
