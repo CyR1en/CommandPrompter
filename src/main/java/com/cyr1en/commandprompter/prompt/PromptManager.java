24df0383a9281a0af211f7924c83cea33179e4ff
CyR1en <cyrien3519@gmail.com>
2022-02-08 00:04:43 -0700

    fix op removal redundancy

--- src/main/java/com/cyr1en/commandprompter/prompt/PromptManager.java
+++ src/main/java/com/cyr1en/commandprompter/prompt/PromptManager.java
@@ -125,8 +125,10 @@ public class PromptManager extends HashMap<String, Class<? extends Prompt>> {
                 sender.setOp(false);
                 plugin.getPluginLogger().debug("Remove OP status");
                 // Redundancy for de-op
-                Bukkit.getScheduler().runTaskLater(plugin, () -> plugin.getPluginLogger()
-                        .debug("Remove OP status (redundancy)"), 1);
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    plugin.getPluginLogger().debug("Remove OP status (redundancy)");
+                    sender.setOp(false);
+                }, 2L);
             }
             promptRegistry.unregister(sender);
         } else if (sender instanceof Player player)
