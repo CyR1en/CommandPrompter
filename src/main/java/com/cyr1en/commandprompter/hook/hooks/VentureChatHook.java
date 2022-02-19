57e74110647be71d9a4be0e8a46152a89905602e
CyR1en <cyrien3519@gmail.com>
2022-02-19 16:15:47 -0700

    actually use VentureChat classes

--- src/main/java/com/cyr1en/commandprompter/hook/hooks/VentureChatHook.java
+++ src/main/java/com/cyr1en/commandprompter/hook/hooks/VentureChatHook.java
@@ -2,33 +2,19 @@ package com.cyr1en.commandprompter.hook.hooks;
 
 import com.cyr1en.commandprompter.CommandPrompter;
 import com.cyr1en.commandprompter.hook.annotations.TargetPlugin;
-import org.bukkit.plugin.Plugin;
-
-import java.util.Collection;
-import java.util.Objects;
+import mineverse.Aust1n46.chat.channel.ChatChannel;
 
 @TargetPlugin(pluginName = "VentureChat")
 public class VentureChatHook {
 
     private final CommandPrompter p;
-    private final Plugin mvPl;
-    private Collection<String> channelAliases;
 
     private VentureChatHook(CommandPrompter plugin) {
         this.p = plugin;
-        mvPl = p.getServer().getPluginManager().getPlugin("VentureChat");
-        extractChannels();
-    }
-
-    private void extractChannels() {
-        var cs = mvPl.getConfig().getConfigurationSection("channels");
-        channelAliases = Objects.requireNonNull(cs).getKeys(false).stream()
-                .map(k -> cs.getString(k + ".alias")).toList();
-        p.getPluginLogger().debug("Channels detected " + channelAliases);
     }
 
     public boolean isChatChannel(String alias) {
-        return channelAliases.contains(alias);
+        return ChatChannel.isChannel(alias);
     }
 
 }
