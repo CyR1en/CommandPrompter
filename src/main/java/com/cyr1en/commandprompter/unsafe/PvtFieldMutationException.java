bbb095a78423cea373a935c8cb664438dd0a5ed9
CyR1en <cyrien3519@gmail.com>
2022-06-06 00:00:15 -0600

    allow to construct using string

--- src/main/java/com/cyr1en/commandprompter/unsafe/PvtFieldMutationException.java
+++ src/main/java/com/cyr1en/commandprompter/unsafe/PvtFieldMutationException.java
@@ -4,4 +4,8 @@ public class PvtFieldMutationException extends RuntimeException {
     public PvtFieldMutationException(Class<?> targetClass) {
         super("Failed to set '" + targetClass.getSimpleName() + "' to private target field");
     }
+
+    public PvtFieldMutationException(String newVal) {
+        super("Failed to set '" + newVal + "' to private target field");
+    }
 }
