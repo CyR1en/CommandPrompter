686004313b77e41737697de116d09f66f7c4b14f
cyr1en <cyrien3519@gmail.com>
2023-08-28 11:22:55 -0600

    optimize imports

--- src/main/java/com/cyr1en/commandprompter/hook/hooks/SuperVanishHook.java
+++ src/main/java/com/cyr1en/commandprompter/hook/hooks/SuperVanishHook.java
@@ -2,7 +2,6 @@ package com.cyr1en.commandprompter.hook.hooks;
 
 import com.cyr1en.commandprompter.CommandPrompter;
 import com.cyr1en.commandprompter.hook.annotations.TargetPlugin;
-import com.cyr1en.commandprompter.prompt.ui.HeadCache;
 import de.myzelyam.api.vanish.PlayerVanishStateChangeEvent;
 import de.myzelyam.api.vanish.VanishAPI;
 import org.bukkit.Bukkit;
@@ -10,8 +9,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-
-import java.util.Objects;
 
 @TargetPlugin(pluginName = "SuperVanish")
 public class SuperVanishHook extends VanishHook implements Listener {
