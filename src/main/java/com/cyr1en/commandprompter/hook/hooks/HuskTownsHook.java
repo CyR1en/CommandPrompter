1c66856ac09d184fc54363c2ef5ca360c0848098
sinoncute <caothanhhien62@gmail.com>
2024-06-30 17:43:10 -0700

    filter only online players

--- src/main/java/com/cyr1en/commandprompter/hook/hooks/HuskTownsHook.java
+++ src/main/java/com/cyr1en/commandprompter/hook/hooks/HuskTownsHook.java
@@ -10,6 +10,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 @TargetPlugin(pluginName = "HuskTowns")
@@ -46,6 +47,7 @@ public class HuskTownsHook extends BaseHook implements FilterHook {
             if (town.get().town().getMembers().isEmpty()) return List.of();
             return town.get().town().getMembers().keySet().stream()
                     .map(Bukkit::getPlayer)
+                    .filter(Objects::nonNull)
                     .toList();
         }
     }
