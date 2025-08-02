c689a26774ff56c93b6660cfede9b95282609aaa
CyR1en <cyrien3519@gmail.com>
2025-08-02 03:22:50 -0600

    update supported versions

--- src/main/java/com/cyr1en/commandprompter/prompt/PromptManager.java
+++ src/main/java/com/cyr1en/commandprompter/prompt/PromptManager.java
@@ -67,7 +67,7 @@ public class PromptManager extends HashMap<String, Class<? extends Prompt>> {
 
     private static final HashMap<Class<? extends Prompt>, Version> supportTable;
 
-    private static final Version LATEST = Version.parse("1.21.7");
+    private static final Version LATEST = Version.parse("1.21.8");
     // Arbitrary 10 version, that means this should work until minecraft v10 lol "any".
     private static final Version ANY = Version.parse("10");
 
@@ -75,7 +75,7 @@ public class PromptManager extends HashMap<String, Class<? extends Prompt>> {
         supportTable = new HashMap<>();
         supportTable.put(ChatPrompt.class, ANY);
         supportTable.put(AnvilPrompt.class, LATEST);
-        supportTable.put(PlayerUIPrompt.class, LATEST);
+        supportTable.put(PlayerUIPrompt.class, Version.parse("1.21.7"));
         supportTable.put(SignPrompt.class, LATEST);
     }
 
