4078bdf42b0dd49d20dd2f430c2ce082fea92ab7
cyr1en <cyrien3519@gmail.com>
2026-06-16 21:48:46 -0600

    chore: ignore IDE and Syncthing files

--- fix_config.py
+++
@@ -1,7 +0,0 @@
-with open('prompt-paper/src/main/java/dev/cyr1en/promptpaper/config/CommandPrompterConfig.java', 'r') as f:
-    text = f.read()
-
-import re
-text = re.sub(r'\s*@ConfigNode\s*@NodeName\("Locale"\).*?String locale\n', '', text, flags=re.DOTALL)
-with open('prompt-paper/src/main/java/dev/cyr1en/promptpaper/config/CommandPrompterConfig.java', 'w') as f:
-    f.write(text)
